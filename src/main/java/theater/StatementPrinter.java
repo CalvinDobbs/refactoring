8fca01bce021d68133529dbe7218c0dfb4e6dce0
Calvin Dobbs <calvin.dobbs@mail.utoronto.ca>
2025-11-24 03:49:02 -0500

    Task 2.1

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -31,9 +31,6 @@ public class StatementPrinter {
 
         for (Performance performance : invoice.getPerformances()) {
 
-            int rslt = 0;
-            rslt = getAmount(performance);
-
             // add volume credits
             volumeCredits += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
             // add extra credit for every five comedy attendees
@@ -45,9 +42,9 @@ public class StatementPrinter {
             result.append(
                     String.format("  %s: %s (%s seats)%n",
                             getPlay(performance).getName(),
-                            frmt.format(rslt / Constants.PERCENT_FACTOR),
+                            frmt.format(getAmount(performance) / Constants.PERCENT_FACTOR),
                             performance.getAudience()));
-            totalAmount += rslt;
+            totalAmount += getAmount(performance);
         }
         result.append(String.format("Amount owed is %s%n",
                 frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
